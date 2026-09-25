3326b6251b46293aad29aac9856e83927ca7ddce
pankajsah152558-creator <pankajsah.152558@marwadiuniversity.ac.in>
2026-09-25 19:11:34 +0530

    Add string replacement functionality in

---
+++ python/unit-2/9.py
@@ -0,0 +1,7 @@
+string = input("Enter a string: ")
+char = input("Character to replace: ")
+replacement = input("Replacement character: ")
+
+new_string = string.replace(char, replacement)
+
+print("Output:", new_string)
