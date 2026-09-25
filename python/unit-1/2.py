22eae68f21530b357ab83f5d5f7658101f7fae8e
pankajsah152558-creator <pankajsah.152558@marwadiuniversity.ac.in>
2026-09-25 10:42:08 +0530

    Refactor modulus calculation and clean output
    Removed unnecessary output statements and modified modulus calculation.

--- python/unit-1/2.py
+++ python/unit-1/2.py
@@ -5,15 +5,4 @@ print("Addition =", a + b)
 print("Subtraction =", a - b)
 print("Multiplication =", a * b)
 print("Division =", a / b)
-print("Modulus =", a % b)
-
-output
-
-Enter first number: 10
-Enter second number: 3
-
-Addition = 13
-Subtraction = 7
-Multiplication = 30
-Division = 3.3333333333333335
-Modulus = 1
+print("Modulus =", a % )
