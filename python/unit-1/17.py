df6799f709f5cb7511219a0cfa4f2495c117a5db
pankajsah152558-creator <pankajsah.152558@marwadiuniversity.ac.in>
2026-09-25 18:49:17 +0530

    Add various number printing patterns in

---
+++ python/unit-1/17.py
@@ -0,0 +1,43 @@
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+  for i in range(1, 6):
+    for j in range(i, 0, -1):
+        print(j, end="")
+    print()
+    for i in range(5, 0, -1):
+    for j in range(i, 6):
+        print(j, end="")
+    print()
+    for i in range(1, 6):
+    for j in range(i, 6):
+        print(j, end="")
+    print()
+    for i in range(5, 0, -1):
+    for j in range(5, i - 1, -1):
+        print(j, end="")
+    print()
+    for i in range(5, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+    for i in range(1, 6):
+    for j in range(5, i - 1, -1):
+        print(j, end="")
+    print()
+    for i in range(5, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end="")
+    print()
+    num = 1
+
+for i in range(1, 6):
+    for j in range(i):
+        print(num, end="")
+        num = num + 1
+    print()
+  for i in range(1, 6):
+    for j in range(i):
+        print(j % 2, end="")
+    print()
