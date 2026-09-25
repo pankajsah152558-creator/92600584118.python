28b3666c23319038a5f6e8e9025d0d0ad8a8bd5d
pankajsah152558-creator <pankajsah.152558@marwadiuniversity.ac.in>
2026-09-25 10:41:21 +0530

    Add basic arithmetic operations in 2.py

---
+++ python/unit-1/2.py
@@ -0,0 +1,19 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
+
+output
+
+Enter first number: 10
+Enter second number: 3
+
+Addition = 13
+Subtraction = 7
+Multiplication = 30
+Division = 3.3333333333333335
+Modulus = 1
